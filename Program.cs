c7b2cf823a5ac9aabab35135397ce35918198c9d
Martin Andersson <martin.andersson@me.com>
2025-10-19 14:05:46 +0200

    Transaction + test i Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
-﻿namespace BudgetTrack
+﻿using BudgetTrack;
+
+namespace BudgetTrack
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            Console.WriteLine("=== Testar Transaction klassen ===");
+
+            //Skapa ett objekt av Transaction
+            Transaction t1 = new Transaction
+            {
+                Id = 1,
+                Description = "Lön",
+                Amount = 25000.0m,
+                Category = "Inkomst",
+                Date = "2025-10-19"
+            };
+
+            // Anropa ShowInfo() för att skriva ut informationen
+            t1.ShowInfo();
+
+            Console.WriteLine("\nTryck valfri tangent för att avsluta....");
+            Console.ReadKey();
+         }
     }
 }
